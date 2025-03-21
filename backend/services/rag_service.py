7b3ee3ae3a8cf1ddd1c7397bd5726e841c141365
Cao1224 <cao916245578@gmail.com>
2025-03-20 17:22:54 -0700

    update backend

--- backend/services/rag_service.py
+++ backend/services/rag_service.py
@@ -8,9 +8,9 @@ from typing import Dict, List, Any, Optional, Tuple
 import re
 from datetime import datetime
 
-__import__('pysqlite3')
+__import__('sqlite3')
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('sqlite3')
 
 import chromadb
 import numpy as np
